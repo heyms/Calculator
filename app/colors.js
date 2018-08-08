4563b022a2f5581ff8e5c9709d5534647f315b0c
Mukesh <mukeshmandan@gmail.com>
2018-08-07 23:11:17 -0700

    Frist commit: Upload Project on Git

---
+++ app/colors.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+import {OPERATION_ADD, OPERATION_SUBTRACT, OPERATION_DIVIDE, OPERATION_MULTIPLY} from './actions/types';
+
+export default {
+  [OPERATION_ADD]: {
+    normal: '#1A237E',
+    darker: '#1A237E',
+    lighter: '#1A237E'
+  },
+  [OPERATION_SUBTRACT]: {
+    normal: '#1A237E',
+    darker: '#1A237E',
+    lighter: '#1A237E'
+  },
+  [OPERATION_DIVIDE]: {
+    normal: '#1A237E',
+    darker: '#1A237E',
+    lighter: '#1A237E'
+  },
+  [OPERATION_MULTIPLY]: {
+    normal: '#1A237E',
+    darker: '#1A237E',
+    lighter: '#1A237E'
+  }
+}
